df68c873963ee7f508edf401e957a707a0192545
General Goods <mainuser@ggw-pos-1.(none)>
2013-12-22 21:09:09 -0600

    init_second_screen.sh space added to fix if statement

--- init_second_screen.sh
+++ init_second_screen.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 SCREEN_COUNT=$($HOME/ggw-pos-standalone/utils/x11_count_displays)
 
-if [ "$SCREEN_COUNT" == "2"]; then
+if [ "$SCREEN_COUNT" == "2" ]; then
 	xsetroot -display :0.1 -cursor_name left_ptr -bg black
 	$HOME/ggw-pos-standalone/init_cv.sh
 	nodewebkit $HOME/ggw-pos-standalone/customerview
