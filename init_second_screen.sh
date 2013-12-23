98c2e9f94c41a820c66638aad2dc456aa0f796fe
Asad Hasan <asadpakistani@gmail.com>
2013-12-22 20:38:34 -0600

    Double screen check migrated to another file triggered from ratpoisonrc

--- init_second_screen.sh
+++ init_second_screen.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
+SCREEN_COUNT=$($HOME/ggw-pos-standalone/utils/x11_count_displays)
 
+if [ "$SCREEN_COUNT" == "2"]; then
+	exec xsetroot -display :0.1 -cursor_name left_ptr -bg black
+	exec $HOME/ggw-pos-standalone/init_cv.sh
+	exec nodewebkit $HOME/ggw-pos-standalone/customerview
+fi
