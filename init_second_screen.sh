8a5b8df4266df76ca48766c6e8f1d7b63866cbe5
Asad Hasan <asadpakistani@gmail.com>
2013-12-22 20:53:27 -0600

    exec removed from second screen script

--- init_second_screen.sh
+++ init_second_screen.sh
@@ -2,7 +2,7 @@
 SCREEN_COUNT=$($HOME/ggw-pos-standalone/utils/x11_count_displays)
 
 if [ "$SCREEN_COUNT" == "2"]; then
-	exec xsetroot -display :0.1 -cursor_name left_ptr -bg black
-	exec $HOME/ggw-pos-standalone/init_cv.sh
-	exec nodewebkit $HOME/ggw-pos-standalone/customerview
+	xsetroot -display :0.1 -cursor_name left_ptr -bg black
+	$HOME/ggw-pos-standalone/init_cv.sh
+	nodewebkit $HOME/ggw-pos-standalone/customerview
 fi
