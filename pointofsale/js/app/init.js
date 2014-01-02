377fd411af9e972c659e226c70d16d4524bc634a
U-Acid-PC\Acid <Acid@Acid-PC.(none)>
2014-01-02 16:00:00 -0600

    D.A. Boilerplate layout added from jsfiddle

--- pointofsale/js/app/init.js
+++ pointofsale/js/app/init.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-	
+	var elements = $('.viewport-main, .right, .left');
+	elements.height($(window).height()-$('.header').height()-$('.footer').height());
+
+	$(window).resize(function() {
+    	elements.height($(window).height()-$('.header').height()-$('.footer').height());
+	});
 });
