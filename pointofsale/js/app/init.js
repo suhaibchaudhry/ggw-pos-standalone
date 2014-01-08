e6761202074279f2be1da5772f6ad31c54f63e8e
Asad Hasan <asadpakistani@gmail.com>
2014-01-08 07:59:27 -0600

    Zepto replace with JQuery 2 for compatibility with Backbone.ModalDialog

--- pointofsale/js/app/init.js
+++ pointofsale/js/app/init.js
@@ -32,20 +32,52 @@ var employeeSession = Backbone.Model.extend({
 	apiServer: 'http://www.general-goods.com'
 });
 
+var employeeLoginDialog = Backbone.ModalView.extend({
+  name: "employeeLoginDialog",
+  templateHtml: _.template("<div class='modal-header'>Add a new person to the list</div>\
+<form>\
+    <table class='compact'>\
+    \
+    <tr><td>\
+        <label for='name'>Name</label>\
+    </td><td>\
+        <input type='text' id='name' />\
+    </td></tr>\
+\
+    <tr><td>\
+        <label for='email'>Email</label>\
+    </td><td>\
+        <input type='text' id='email' />\
+    </td></tr>\
+    \
+    <tr><td>\
+        <label for='phone'>Phone</label>\
+    </td><td>\
+        <input type='text' id='phone' />\
+    </td></tr>\
+    \
+    <tr><td></td><td>\
+        <input id='addPersonButton' type='submit' value='Add person' />\
+    </td></tr>\
+    \
+    </table>\
+</form>"),
+});
+
 var applicationFrame = Backbone.View.extend({
 	tagName: 'div',
 	initialize: function() {
     this.employeeSession = new employeeSession();
+    this.employeeLoginDialog = new employeeLoginDialog();
 		this.listenTo(this.employeeSession, 'change:token', this.render);
     this.employeeSession.initialSession();
 	},
 	render: function(model, value, options) {
 		if(value) {
 			console.log('login true');
-			alert('login true');
+			//alert('login true');
 		} else {
-			console.log('login false');
-			alert('login false');
+      this.employeeLoginDialog.render().showModal();	
 		}
 
 		return this;
