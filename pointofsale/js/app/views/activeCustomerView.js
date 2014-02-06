f314c9953a40d8528fcdc9642a735e79761579bd
Asad Hasan <asadpakistani@gmail.com>
2014-02-06 00:22:08 -0600

    Next instruction added to active cutomer view.

--- pointofsale/js/app/views/activeCustomerView.js
+++ pointofsale/js/app/views/activeCustomerView.js
@@ -1,12 +1,13 @@
 jQuery(function($) {
 	activeCustomerView = Backbone.View.extend({
+		//Implement ActiveCustomer model, and typeahead customer search.
 		tagName: 'div',
 		searchBoxTemplate: _.template($('#customer-search-components').html()),
 		render: function() {
 			this.$('.customer-search').append(this.searchBoxTemplate());
 		},
 		demolish: function() {
-			
+
 		}
 	});
 });
