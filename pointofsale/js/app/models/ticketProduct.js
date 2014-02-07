8e05eccf757ef472d8fdfa190ce99455f12bdee1
Asad Hasan <asadpakistani@gmail.com>
2014-02-07 11:01:56 -0600

    Customer aware prices working. Need testing.

--- pointofsale/js/app/models/ticketProduct.js
+++ pointofsale/js/app/models/ticketProduct.js
@@ -3,23 +3,57 @@ jQuery(function($) {
   ticketProduct = Backbone.Model.extend({
   	initialize: function(attributes, options) {
       var activeCustomer = this.get('activeTicketView').activeCustomerView.activeCustomer;
-  		if(activeCustomer.get('id')) {
-        //Perform role checks, listen on active customer for changing roles.
+  		console.log(activeCustomer.attributes);
+      console.log(this.attributes);
+      if(activeCustomer.get('id')) {
+        //Perform role checks.
         this.set('retail', false);
-        this.set('price', 25);
+        this.set('price', this.getRolePrice());
       } else {
         this.set('retail', true);
         this.set('price', attributes['sell_price']);
       }
 
+      //listen on active customer for changing customers.
       this.listenTo(activeCustomer, 'change:id', this.customerChanged);
   	},
+    customerHasRole: function(rid) {
+      var activeCustomer = this.get('activeTicketView').activeCustomerView.activeCustomer;
+      var roleExists = false;
+      _.each(activeCustomer.get('roles'), function(role) {
+        if(role == rid) {
+          roleExists = true;
+          return false;
+        }
+      });
+
+      return roleExists;
+    },
+    getRolePrice: function() {
+      var activeCustomer = this.get('activeTicketView').activeCustomerView.activeCustomer;
+      var roles = activeCustomer.get('roles');
+      var min_role_price = 0;
+      var product = this;
+      _.each(this.get('special_prices'), function(spo) {
+        if(product.customerHasRole(spo.role)) {
+          var price = accounting.unformat(spo.price);
+          if(min_role_price == 0 || price < min_role_price) {
+            min_role_price = price;
+          }
+        }
+      });
+
+      if(min_role_price == 0) {
+        return this.get('sell_price');
+      } else {
+        return min_role_price;
+      }
+    },
     customerChanged: function(model, value, options) {
-      console.log('Changed');
       if(value) {
         //Perform role checks, listen on active customer for changing roles.
         this.set('retail', false);
-        this.set('price', 25);
+        this.set('price', this.getRolePrice());
       } else {
         this.set('retail', true);
         this.set('price', attributes['sell_price']);
