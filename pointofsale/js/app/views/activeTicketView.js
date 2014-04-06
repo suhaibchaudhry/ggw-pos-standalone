036146b7df64e8474c9e8914d92e1b63209cb22f
Asad Hasan <asadpakistani@gmail.com>
2014-04-06 01:30:28 -0500

    Manually focus scan on remove item.

--- pointofsale/js/app/views/activeTicketView.js
+++ pointofsale/js/app/views/activeTicketView.js
@@ -151,7 +151,8 @@ jQuery(function($) {
     },
     removeLineItem: function(e) {
       e.preventDefault();
-      e.stopPropagation(); //Stop Propagation to avoid dialog behind bug.
+      //e.stopPropagation(); //Allow propagation to select text box.
+      this.activateScanFocus();
       this.ticket.removeItem(e.currentTarget.parentNode.parentNode.dataset.id);
     },
     incrementQty: function(e) {
