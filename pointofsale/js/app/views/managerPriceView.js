bf5a8b7622c8f33f3f7748f9b03922d203864d2a
Asad Hasan <asadpakistani@gmail.com>
2014-04-15 10:04:03 -0500

    Qty split and override form added

--- pointofsale/js/app/views/managerPriceView.js
+++ pointofsale/js/app/views/managerPriceView.js
@@ -5,7 +5,9 @@ jQuery(function($) {
     className: 'managerPrice',
     events: {
       "click a.price-override-cancel": "cancelOverride",
-      "click a.price-override-continue": "continueOverride"
+      "click a.price-override-continue": "continueOverride",
+      "click .price-override-settings .qty-split a.incr": "increaseQtySplit",
+      "click .price-override-settings .qty-split a.decr": "decreaseQtySplit"
     },
     labelizeData: _.template($('#labelize-data').html()),
     priceOverrideSettings: _.template($('#price-override-settings-form').html()),
@@ -29,6 +31,7 @@ jQuery(function($) {
       var id = line_item.dataset.id;
       var product = this.ticket.get('productCollection').get(id);
       if(product) {
+        this.product = product;
         this.modal.display(true);
 
         this.$('.price-override-settings').html(this.priceOverrideSettings({
@@ -44,6 +47,32 @@ jQuery(function($) {
     continueOverride: function(e) {
       e.preventDefault();
       this.modal.display(false);
+    },
+    increaseQtySplit: function(e) {
+      e.preventDefault();
+      this.QtySplitModify(true);
+    },
+    decreaseQtySplit: function(e) {
+      e.preventDefault();
+      this.QtySplitModify(false);
+    },
+    QtySplitModify: function(incr) {
+      var price = this.product.get('price');
+      var qty_split = accounting.unformat(this.$('span.split-value').text());
+      if(incr) {
+        qty_split++;
+        this.priceModify(price, qty_split);
+      } else {
+        if(qty_split > 1) {
+          qty_split--;
+          this.priceModify(price, qty_split);
+        }
+      }
+    },
+    priceModify: function(price, qty_split) {
+      price = price/qty_split;
+      this.$('input.overriden-price').val(accounting.formatNumber(price, 2, "", "."));
+      this.$('span.split-value').text(qty_split);
     }
   });
 });
