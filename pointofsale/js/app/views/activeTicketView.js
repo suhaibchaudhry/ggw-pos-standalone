e8218f99c4c7533fd54e977b77371b828d7aa609
Asad Hasan <asadpakistani@gmail.com>
2014-07-29 21:59:24 -0500

    Add confirmation dialog on removal of a product.

--- pointofsale/js/app/views/activeTicketView.js
+++ pointofsale/js/app/views/activeTicketView.js
@@ -177,7 +177,15 @@ jQuery(function($) {
     removeLineItem: function(e) {
       e.preventDefault();
       e.stopPropagation(); //Allow propagation to select text box.
-      this.ticket.removeItem(e.currentTarget.parentNode.parentNode.dataset.id);
+      var name = $('.name', e.currentTarget.parentNode.parentNode).text();
+      var packaging = $('.packaging', e.currentTarget.parentNode.parentNode).text();
+      var confirm_message = "Are you sure you want to remove '"+name+"'";
+      if(packaging) {
+        confirm_message += " - "+packaging;
+      }
+      if(confirm(confirm_message)) {
+        this.ticket.removeItem(e.currentTarget.parentNode.parentNode.dataset.id);
+      }
     },
     incrementQty: function(e) {
       e.preventDefault();
