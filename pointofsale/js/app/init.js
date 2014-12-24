47d131292f92e4c6ff0973a570eeaa395e1357e4
Asad Hasan <asadpakistani@gmail.com>
2014-12-24 04:41:25 -0600

    Setup defaultsi for alertify on pos init.

--- pointofsale/js/app/init.js
+++ pointofsale/js/app/init.js
@@ -13,6 +13,50 @@ jQuery(function($) {
     calculatorSkin: _.template($('#dash-calculator').html()),
     addCustomer: _.template($('#add-customer').html()),
   	initialize: function() {
+      //Setup Alertify
+      alertify.defaults = {
+          // dialogs defaults
+          modal: true,
+          movable: true,
+          resizable: true,
+          closable: true,
+          maximizable: true,
+          pinnable: true,
+          pinned: true,
+          padding: true,
+          overflow: true,
+          maintainFocus: false,
+          transition:'pulse',
+
+          // notifier defaults
+          notifier:{
+              // auto-dismiss wait time (in seconds)  
+              delay: 5,
+              // default position
+              position:'bottom-right'
+          },
+
+          // language resources 
+          glossary:{
+              // dialogs default title
+              title:'General Goods Wholesale',
+              // ok button text
+              ok: 'OK',
+              // cancel button text
+              cancel: 'Cancel'
+          },
+
+          // theme settings
+          theme: {
+              // class name attached to prompt dialog input textbox.
+              input:'ajs-input',
+              // class name attached to ok button
+              ok:'ajs-ok',
+              // class name attached to cancel button 
+              cancel:'ajs-cancel'
+          }
+      };
+
       //Global checkout locking flag
       checkoutActive = false;
 
