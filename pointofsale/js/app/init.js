2bcb121be04c69d1c03da40946c6abc5d9e207db
Asad Hasan <asadpakistani@gmail.com>
2014-12-24 12:12:02 -0600

    Temporarily set import1.general-goods.com

--- pointofsale/js/app/init.js
+++ pointofsale/js/app/init.js
@@ -61,7 +61,7 @@ jQuery(function($) {
       checkoutActive = false;
 
       //Employee Session Model
-      this.employeeSession = new employeeSession({apiServer: 'http://test.general-goods.com:7000'});
+      this.employeeSession = new employeeSession({apiServer: 'http://import1.general-goods.com'});
       this.activeCustomer = new activeCustomer();
       this.preloaderSemaphore = 0;
       this.checkoutHideSemaphore = 0;
