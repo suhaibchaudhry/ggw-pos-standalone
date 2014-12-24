2f9a40f2435851fdaf604c101856277d84f70530
Asad Hasan <asadpakistani@gmail.com>
2014-12-24 13:00:39 -0600

    Configure alertjs for inventory view.

--- pointofsale/js/app/init_inventory.js
+++ pointofsale/js/app/init_inventory.js
@@ -9,6 +9,50 @@ jQuery(function($) {
       window.close();
     },
   	initialize: function() {
+      //Setup Alertify
+      alertify.defaults = {
+          // dialogs defaults
+          modal: true,
+          movable: true,
+          resizable: true,
+          closable: true,
+          maximizable: true,
+          pinnable: true,
+          pinned: true,
+          padding: true,
+          overflow: true,
+          maintainFocus: false,
+          transition:'pulse',
+
+          // notifier defaults
+          notifier:{
+              // auto-dismiss wait time (in seconds)  
+              delay: 5,
+              // default position
+              position:'bottom-right'
+          },
+
+          // language resources 
+          glossary:{
+              // dialogs default title
+              title:'General Goods Wholesale',
+              // ok button text
+              ok: 'OK',
+              // cancel button text
+              cancel: 'Cancel'
+          },
+
+          // theme settings
+          theme: {
+              // class name attached to prompt dialog input textbox.
+              input:'ajs-input',
+              // class name attached to ok button
+              ok:'ajs-ok',
+              // class name attached to cancel button 
+              cancel:'ajs-cancel'
+          }
+      };
+
       this.preloaderSemaphore = 0;
       this.activeInventoryRegion = new activeInventoryView({
         appFrame: this,
