ee240a73f946b91bf17ccb1269ffb3a3f37bd68c
Asad Hasan <asad@dadesigners.com>
2015-02-02 16:26:57 -0600

    Update jquery card swipe script.

--- pointofsale/js/vendor/jquery.cardswipe.js
+++ pointofsale/js/vendor/jquery.cardswipe.js
@@ -18,10 +18,39 @@
 }(function ($) {
 
 	// State definitions:
-	var states = { IDLE: 0, PENDING: 1, READING: 2, DISCARD: 3 };
+	var states = { IDLE: 0, PENDING: 1, READING: 2, DISCARD: 3, PREFIX: 4 };
 
-	// Holds current state
-	var state = states.IDLE;
+	// State names used when debugging.
+	var stateNames = { 0: 'IDLE', 1: 'PENDING', 2: 'READING', 3: 'DISCARD', 4: 'PREFIX' };
+
+	// Holds current state. Update only through state function.
+	var currentState = states.IDLE;
+
+	// Gets or sets the current state.
+	var state = function() {
+
+		if (arguments.length > 0) {
+			// Set new state
+			var newState = arguments[0];
+			if (newState == state)
+				return;
+
+			if (settings.debug) {
+				console.log("%s -> %s", stateNames[currentState], stateNames[newState]);
+			}
+
+			if (settings.debugElement) {
+				settings.debugElement.append(stateNames[currentState] + ' -> ' + stateNames[newState]);
+			}
+
+			currentState = newState;
+		}
+		else {
+			// Get current state
+			return currentState;
+		}
+
+	};
 
 	// Array holding scanned characters
 	var scanbuffer;
@@ -31,11 +60,14 @@
 
 	// Keypress listener
 	var listener = function (e) {
-		switch (state) {
+		settings.debug && console.log(e.which + ': ' + String.fromCharCode(e.which));
+		switch (state()) {
+
+			// IDLE: Look for '%', and jump to PENDING.  Otherwise, pass the keypress through.
 			case states.IDLE:
 				// Look for '%'
 				if (e.which == 37) {
-					state = states.PENDING;
+					state(states.PENDING);
 					scanbuffer = new Array();
 					processCode(e.which);
 					e.preventDefault();
@@ -43,13 +75,22 @@
 					startTimer();
 				}
 
+				// Look for prefix, if defined
+				if (settings.prefixCode && settings.prefixCode == e.which) {
+					state(states.PREFIX);
+					e.preventDefault();
+					e.stopPropagation();
+					startTimer();
+				}
+
 				break;
 
+			// PENDING: Look for A-Z, then jump to READING.  Otherwise, pass the keypress through, reset and jump to IDLE.
 			case states.PENDING:
 				// Look for format code character, A-Z. Almost always B for cards
 				// used by the general public.
 				if (e.which >= 65 && e.which <= 90) {
-					state = states.READING;
+					state(states.READING);
 
 					// Leaving focus on a form element wreaks browser-dependent
 					// havoc because of keyup and keydown events.  This is a
@@ -64,10 +105,11 @@
 				else {
 					clearTimer();
 					scanbuffer = null;
-					state = states.IDLE;
+					state(states.IDLE);
 				}
 				break;
 
+			// READING: Copy characters to buffer until newline, then process the scanned characters
 			case states.READING:
 				processCode(e.which);
 				startTimer();
@@ -77,35 +119,56 @@
 				// Carriage return indicates end of scan
 				if (e.which == 13) {
 					clearTimer();
-					state = states.IDLE;
+					state(states.IDLE);
 					processScan();
 				}
 
 				if (settings.firstLineOnly && e.which == 63) {
 					// End of line 1.  Return early, and eat remaining characters.
-					state = states.DISCARD;
+					state(states.DISCARD);
 					processScan();
 				}
 				break;
 
+			// DISCARD: Eat up characters until newline, then jump to IDLE
 			case states.DISCARD:
 				e.preventDefault();
 				e.stopPropagation();
 				if (e.which == 13) {
 					clearTimer();
-					state = states.IDLE;
+					state(states.IDLE);
 					return;
 				}
 
 				startTimer();
 				break;
+
+			// PREFIX: Eat up characters until % is seen, then jump to PENDING
+			case states.PREFIX:
+				settings.debug && console.log('PREFIX');
+
+				// If prefix character again, pass it through and return to IDLE state.
+				if (e.which == settings.prefixCode) {
+					state(states.IDLE);
+					return;
+				}
+
+				// Eat character.
+				e.preventDefault();
+				e.stopPropagation();
+				// Look for '%'
+				if (e.which == 37) {
+					state(states.PENDING);
+					scanbuffer = new Array();
+					processCode(e.which);
+				}
+				startTimer();
 		}
 	};
 
 	// Converts a scancode to a character and appends it to the buffer.
 	var processCode = function (code) {
 		scanbuffer.push(String.fromCharCode(code));
-		//console.log(code);
 	}
 
 	var startTimer = function () {
@@ -120,16 +183,21 @@
 
 	// Invoked when the timer lapses.
 	var onTimeout = function () {
-		if (state == states.READING) {
+		settings.debug && console.log('Timeout!');
+		if (state() == states.READING) {
 			processScan();
 		}
 		scanbuffer = null;
-		state = states.IDLE;
+		state(states.IDLE);
 	};
 
 
 	// Processes the scanned card
 	var processScan = function () {
+
+		if (settings.debug) {
+			console.log(scanbuffer);
+		}
 
 		var rawData = scanbuffer.join('');
 
@@ -183,7 +251,9 @@
 		success: defaultSuccessCallback,
 		error: null,
 		parser: defaultParser,
-		firstLineOnly: false
+		firstLineOnly: false,
+		prefixCharacter: null,
+		debug: false,
 	};
 
 	// Plugin actual settings
@@ -194,6 +264,15 @@
 	var methods = {
 		init: function (options) {
 			settings = $.extend(defaults, options || {});
+
+			// Is a prefix character defined?
+			if (settings.prefixCharacter) {
+				if (settings.prefixCharacter.length != 1)
+					throw 'PrefixCharacter must be  a single character';
+
+				// Convert to character code
+				settings.prefixCode = settings.prefixCharacter.charCodeAt(0);
+			}
 
 			if (settings.enabled)
 				methods.enable();
@@ -211,15 +290,16 @@
 
 	// The extension proper.  Dispatches methods using the usual jQuery pattern.
 	$.cardswipe = function (method) {
-		// Method calling logic
+		// Method calling logic. If named method exists, execute it with passed arguments
 		if (methods[method]) {
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 		}
+		// If no argument, or an object passed, invoke init method.
 		else if (typeof method === 'object' || !method) {
 			return methods.init.apply(this, arguments);
 		}
 		else {
-			$.error('Method ' + method + ' does not exist on jQuery.cardswipe');
+			throw 'Method ' + method + ' does not exist on jQuery.cardswipe';
 		}
 	}
 
